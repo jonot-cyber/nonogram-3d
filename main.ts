96ae9448c4ddbf539a3a31a1ec021416346fadfc
jonot <48457507+jonot-cyber@users.noreply.github.com>
2023-08-31 20:54:24 +0000

    Improve lighting

--- main.ts
+++ main.ts
@@ -1,23 +1,30 @@
-import * as THREE from 'three';
+import { BoxGeometry, DirectionalLight, Mesh, MeshLambertMaterial, PerspectiveCamera, Scene, WebGLRenderer } from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const scene = new Scene();
+const camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+const renderer = new WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+const controls = new OrbitControls(camera, renderer.domElement);
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00});
-const cube = new THREE.Mesh(geometry, material);
+const geometry = new BoxGeometry(1, 1, 1);
+const material = new MeshLambertMaterial({ color: 0xffffff });
+const cube = new Mesh(geometry, material);
 scene.add(cube);
+
+const directionalLight = new DirectionalLight(0xffffff, 1);
+scene.add(directionalLight);
+scene.add(directionalLight.target);
+directionalLight.target.position.setX(0.5);
+directionalLight.target.position.setY(0);
+directionalLight.target.position.setZ(-0.2);
 
 camera.position.z = 5;
 
 function animate() {
     requestAnimationFrame(animate);
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
     renderer.render(scene, camera);
 }
 
