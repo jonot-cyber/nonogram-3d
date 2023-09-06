d7f6edba0d83ae0f8ec845e74a92127210ad95d4
jonot <48457507+jonot-cyber@users.noreply.github.com>
2023-09-06 14:08:22 +0000

    Fix flagging and visibility

--- main.ts
+++ main.ts
@@ -33,7 +33,7 @@ function isVisible(xray: XRay, x: number, y: number, z: number, maxX: number, ma
     return true;
 }
 
-function getAssetURL(hint: Hint): string {
+function getAssetURL(hint: Hint, painted: boolean = false): string {
     let number = hint.count.toString();
     let type = "";
     if (hint.type == "circle") {
@@ -41,10 +41,11 @@ function getAssetURL(hint: Hint): string {
     } else if (hint.type == "square") {
         type = "s";
     }
-    return `/assets/${number}${type}w.png`
+    return `/assets/${number}${type}${painted ? "p" : "w"}.png`
 }
 
 let click = false;
+let flag = false;
 let moved = false;
 const pointer = new Vector2();
 
@@ -110,7 +111,8 @@ const puzzle: Puzzle = [
         [false, false, false, false, false],
     ],
 ];
-const puzzleSize = { x: puzzle.length, y: puzzle[0].length, z: puzzle[0][0].length};
+const loader = new TextureLoader();
+const puzzleSize = { x: puzzle.length, y: puzzle[0].length, z: puzzle[0][0].length };
 const distance = Math.sqrt(puzzleSize.x * puzzleSize.x + puzzleSize.y * puzzleSize.y + puzzleSize.z * puzzleSize.z);
 camera.position.z = distance;
 const hints: Hints = createHints(puzzle);
@@ -118,11 +120,7 @@ const cubes: Mesh[] = [];
 for (let x = 0; x < puzzleSize.x; x++) {
     for (let y = 0; y < puzzleSize.y; y++) {
         for (let z = 0; z < puzzleSize.z; z++) {
-            if (!isVisible(xray, x, y, z, puzzleSize.x, puzzleSize.y, puzzleSize.z)) {
-                continue;
-            }
             const geometry = new BoxGeometry(1, 1, 1);
-            const loader = new TextureLoader();
             const materials = [
                 new MeshLambertMaterial({ map: loader.load(getAssetURL(hints.x[y][z])) }), // right
                 new MeshLambertMaterial({ map: loader.load(getAssetURL(hints.x[y][z])) }), // left
@@ -133,6 +131,11 @@ for (let x = 0; x < puzzleSize.x; x++) {
             ];
             const cube = new Mesh(geometry, materials);
             cube.position.set(x - puzzleSize.x / 2 + 0.5, y - puzzleSize.y / 2 + 0.5, z - puzzleSize.z / 2 + 0.5);
+            cube.qX = x;
+            cube.qY = y;
+            cube.qZ = z;
+            cube.qFlag = false;
+            cube.layers.enable(0);
             scene.add(cube);
             cubes.push(cube);
         }
@@ -158,32 +161,67 @@ directionalLight2.target.position.setZ(0.2);
 function animate() {
     requestAnimationFrame(animate);
     if (click) {
+        click = false;
         raycaster.setFromCamera(pointer, camera);
+        raycaster.layers.set(0);
 
         const intersects = raycaster.intersectObjects(scene.children);
         if (intersects.length > 0) {
+            if (intersects[0].object.qFlag) {
+                return;
+            }
             scene.remove(intersects[0].object);
         }
-        click = false;
+    }
+    if (flag) {
+        flag = false;
+        raycaster.setFromCamera(pointer, camera);
+        raycaster.layers.set(0);
+
+        const intersects = raycaster.intersectObjects(scene.children);
+        if (intersects.length > 0) {
+            let o: Mesh = intersects[0].object as Mesh;
+            o.qFlag = !o.qFlag;
+            let x: number = o.qX;
+            let y: number = o.qY;
+            let z: number = o.qZ;
+            o.material = [
+                new MeshLambertMaterial({ map: loader.load(getAssetURL(hints.x[y][z], o.qFlag)) }),
+                new MeshLambertMaterial({ map: loader.load(getAssetURL(hints.x[y][z], o.qFlag)) }),
+                new MeshLambertMaterial({ map: loader.load(getAssetURL(hints.y[x][z], o.qFlag)) }),
+                new MeshLambertMaterial({ map: loader.load(getAssetURL(hints.y[x][z], o.qFlag)) }),
+                new MeshLambertMaterial({ map: loader.load(getAssetURL(hints.z[x][y], o.qFlag)) }),
+                new MeshLambertMaterial({ map: loader.load(getAssetURL(hints.z[x][y], o.qFlag)) }),
+            ];
+        }
     }
     renderer.render(scene, camera);
 }
 
-window.addEventListener('mousedown', function (ev: MouseEvent) {
+window.addEventListener("mousedown", function (ev: MouseEvent) {
     moved = false;
 })
 
-window.addEventListener('mousemove', function (ev: MouseEvent) {
+window.addEventListener("mousemove", function (ev: MouseEvent) {
     moved = true;
+    pointer.x = (ev.clientX / window.innerWidth) * 2 - 1;
+    pointer.y = - (ev.clientY / window.innerHeight) * 2 + 1;
 })
 
-window.addEventListener('mouseup', function (ev: MouseEvent) {
+window.addEventListener("mouseup", function (ev: MouseEvent) {
     if (moved) {
         return;
     }
     pointer.x = (ev.clientX / window.innerWidth) * 2 - 1;
     pointer.y = - (ev.clientY / window.innerHeight) * 2 + 1;
     click = true;
+})
+
+window.addEventListener("keydown", function (ev: KeyboardEvent) {
+    if (ev.key != "f") {
+        return;
+    }
+    flag = true;
 })
 
 const cropDir = document.querySelector<HTMLSelectElement>("#crop-dir");
@@ -198,7 +236,13 @@ function changeHandle(e: Event) {
     let y = 0;
     let z = 0;
     for (let cube of cubes) {
-        cube.visible = isVisible(xray, x, y, z, puzzleSize.x, puzzleSize.y, puzzleSize.z);
+        if (isVisible(xray, x, y, z, puzzleSize.x, puzzleSize.y, puzzleSize.z)) {
+            cube.visible = true;
+            cube.layers.enable(0);
+        } else {
+            cube.visible = false;
+            cube.layers.disable(0);
+        }
         z++;
         if (z == puzzleSize.z) {
             y++;
