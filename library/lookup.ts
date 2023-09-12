7f50aeef193be8b265c3b04448a34a25d1e7ddb5
jonot <48457507+jonot-cyber@users.noreply.github.com>
2023-09-12 17:27:34 +0000

    Refactor some code

---
+++ library/lookup.ts
@@ -0,0 +1,15 @@
+export const puzzleTable = {
+    "mug": "/library/mug.json",
+    "hund": "/library/hund.json",
+    "shibainu": "/library/shibaInu.json",
+    "note": "/library/note.json",
+    "flag": "/library/flag.json",
+    "palmTree": "/library/palmTree.json",
+    "hand": "/library/hand.json",
+    "youtube": "/library/youtube.json",
+    "j": "/library/j.json",
+    "o": "/library/o.json",
+    "e": "/library/e.json",
+    "t": "/library/t.json",
+    "worm": "/library/worm.json",
+}
--- main.ts
+++ main.ts
@@ -2,6 +2,8 @@ import { BoxGeometry, Color, DirectionalLight, Mesh, MeshLambertMaterial, Octahe
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { Hint, Hints, Puzzle, createHints } from './puzzle';
 import { removeHints } from './reduce';
+import { puzzleTable } from './library/lookup';
+import { clamp } from 'three/src/math/MathUtils';
 
 const debug = {
     showShape: false,
@@ -17,12 +19,9 @@ interface XRay {
     count: number;
 }
 
-let xray: XRay = {
-    direction: "up",
-    count: 0,
-};
+const loader = new TextureLoader();
 
-function updateMaterial(mesh: CoolMesh) {
+function updateMaterial(mesh: CoolMesh, loader: TextureLoader, hints: Hints) {
     let x = mesh.qX ?? 0;
     let y = mesh.qY ?? 0;
     let z = mesh.qZ ?? 0;
@@ -37,10 +36,13 @@ function updateMaterial(mesh: CoolMesh) {
 
 }
 
-function isVisible(xray: XRay, x: number, y: number, z: number, maxX: number, maxY: number, maxZ: number): boolean {
+function isVisible(xray: XRay, cube: CoolMesh, maxX: number, maxY: number, maxZ: number): boolean {
     if (xray.count == 0) {
         return true;
     }
+    const x = cube.qX ?? 0;
+    const y = cube.qY ?? 0;
+    const z = cube.qZ ?? 0;
     switch (xray.direction) {
         case "up":
             return maxY - y > xray.count;
@@ -92,41 +94,28 @@ controls.enablePan = false;
 controls.enableZoom = false;
 
 // I create two lights in opposite directions to make it so you can look at any angle, and it still looks good.
-const directionalLight = new DirectionalLight(0xffffff, 1);
-scene.add(directionalLight);
-scene.add(directionalLight.target);
-directionalLight.target.position.setX(0.5);
-directionalLight.target.position.setY(0);
-directionalLight.target.position.setZ(-0.2);
-
-const directionalLight2 = new DirectionalLight(0xffffff, 1);
-scene.add(directionalLight2);
-scene.add(directionalLight2.target);
-directionalLight2.target.position.setX(-0.5);
-directionalLight2.target.position.setY(2);
-directionalLight2.target.position.setZ(0.2);
+function createLights() {
+    const directionalLight = new DirectionalLight(0xffffff, 1);
+    scene.add(directionalLight);
+    scene.add(directionalLight.target);
+    directionalLight.target.position.setX(0.5);
+    directionalLight.target.position.setY(0);
+    directionalLight.target.position.setZ(-0.2);
+    
+    const directionalLight2 = new DirectionalLight(0xffffff, 1);
+    scene.add(directionalLight2);
+    scene.add(directionalLight2.target);
+    directionalLight2.target.position.setX(-0.5);
+    directionalLight2.target.position.setY(2);
+    directionalLight2.target.position.setZ(0.2);
+}
+createLights();
 
 const urlParams = new URLSearchParams(window.location.search);
 let puzzleName = urlParams.get("puzzle");
-const puzzleTable = {
-    "mug": "/library/mug.json",
-    "hund": "/library/hund.json",
-    "shibainu": "/library/shibaInu.json",
-    "note": "/library/note.json",
-    "flag": "/library/flag.json",
-    "palmTree": "/library/palmTree.json",
-    "hand": "/library/hand.json",
-    "youtube": "/library/youtube.json",
-    "j": "/library/j.json",
-    "o": "/library/o.json",
-    "e": "/library/e.json",
-    "t": "/library/t.json",
-    "worm": "/library/worm.json",
-}
 let response = await fetch(puzzleTable[puzzleName ?? ""]);
 let json = await response.json();
 const puzzle: Puzzle = json.puzzle;
-const loader = new TextureLoader();
 const puzzleSize = { x: puzzle.length, y: puzzle[0].length, z: puzzle[0][0].length };
 const distance = Math.sqrt(puzzleSize.x * puzzleSize.x + puzzleSize.y * puzzleSize.y + puzzleSize.z * puzzleSize.z);
 camera.position.z = distance;
@@ -136,39 +125,48 @@ if (debug.reduceHints) {
     console.log(JSON.stringify(hints));
 }
 const cubes: Mesh[] = [];
-for (let x = 0; x < puzzleSize.x; x++) {
-    for (let y = 0; y < puzzleSize.y; y++) {
-        for (let z = 0; z < puzzleSize.z; z++) {
-            if (!puzzle[x][y][z] && debug.showShape) {
-                continue;
+
+function createCubes(size: { x: number, y: number, z: number }, puzzle: Puzzle) {
+    for (let x = 0; x < size.x; x++) {
+        for (let y = 0; y < size.y; y++) {
+            for (let z = 0; z < size.z; z++) {
+                if (!puzzle[x][y][z] && debug.showShape) {
+                    continue;
+                }
+                const geometry = new BoxGeometry(1, 1, 1);
+                const cube: CoolMesh = new Mesh(geometry);
+                cube.position.set(x - puzzleSize.x / 2 + 0.5, y - puzzleSize.y / 2 + 0.5, z - puzzleSize.z / 2 + 0.5);
+                cube.qX = x;
+                cube.qY = y;
+                cube.qZ = z;
+                cube.qFlag = false;
+                cube.layers.enable(0);
+                scene.add(cube);
+                updateMaterial(cube, loader, hints);
+                cubes.push(cube);
             }
-            const geometry = new BoxGeometry(1, 1, 1);
-            const cube: CoolMesh = new Mesh(geometry);
-            cube.position.set(x - puzzleSize.x / 2 + 0.5, y - puzzleSize.y / 2 + 0.5, z - puzzleSize.z / 2 + 0.5);
-            cube.qX = x;
-            cube.qY = y;
-            cube.qZ = z;
-            cube.qFlag = false;
-            cube.layers.enable(0);
-            scene.add(cube);
-            updateMaterial(cube);
-            cubes.push(cube);
         }
     }
 }
+createCubes(puzzleSize, puzzle);
+
+const handleMinX = -puzzleSize.x / 2 - 1;
+const handleMaxX = handleMinX + puzzleSize.x - 1;
+const handleMinZ = -puzzleSize.z / 2 - 1;
+const handleMaxZ = handleMinX + puzzleSize.z - 1;
 
 const handleGeometry = new OctahedronGeometry(0.25);
 const xHandleMesh = new Mesh(handleGeometry, new MeshLambertMaterial({ color: 0xff00ff, opacity: 0.5, transparent: true }));
 scene.add(xHandleMesh);
-xHandleMesh.position.set(-puzzleSize.x / 2 - 1, -puzzleSize.y / 2, -puzzleSize.z / 2);
+xHandleMesh.position.set(handleMinX, -puzzleSize.y / 2, -puzzleSize.z / 2);
 xHandleMesh.scale.set(2, 1, 1);
-let xOriginalPosition: Vector3 = new Vector3(xHandleMesh.position.x, xHandleMesh.position.y, xHandleMesh.position.z);
+let xOriginalPosition = xHandleMesh.position.x;
 
 const zHandleMesh = new Mesh(handleGeometry, new MeshLambertMaterial({ color: 0xffff00, opacity: 0.5, transparent: true }));
 scene.add(zHandleMesh);
-zHandleMesh.position.set(-puzzleSize.x / 2, -puzzleSize.y / 2, -puzzleSize.z / 2 - 1);
+zHandleMesh.position.set(-puzzleSize.x / 2, -puzzleSize.y / 2, handleMinZ);
 zHandleMesh.scale.set(1, 1, 2);
-let zOriginalPosition: Vector3 = new Vector3(zHandleMesh.position.x, zHandleMesh.position.y, zHandleMesh.position.z);
+let zOriginalPosition = zHandleMesh.position.z;
 
 renderer.domElement.addEventListener("mousemove", function (ev: MouseEvent) {
     pointer.x = (ev.clientX / window.innerWidth) * 2 - 1;
@@ -180,13 +178,14 @@ renderer.domElement.addEventListener("mousedown", function (ev: MouseEvent) {
 })
 
 renderer.domElement.addEventListener("mouseup", function (ev: MouseEvent) {
-    if (mouseOnHandle != null) {
-        controls.enableRotate = true;
-        xHandleMesh.material.opacity = 0.5;
-        zHandleMesh.material.opacity = 0.5;
-    }
-    mouseOnHandle = null;
     ev.preventDefault();
+    if (mouseOnHandle == null) {
+        return;
+    }
+    controls.enableRotate = true;
+    xHandleMesh.material.opacity = 0.5;
+    zHandleMesh.material.opacity = 0.5;
+    mouseOnHandle = null;
 })
 
 window.addEventListener("keydown", function (ev: KeyboardEvent) {
@@ -215,54 +214,12 @@ window.addEventListener("keyup", function (ev: KeyboardEvent) {
     }
 })
 
-const cropDir = document.querySelector<HTMLSelectElement>("#crop-dir");
-const cropCount = document.querySelector<HTMLInputElement>("#crop-count");
-
-function changeHandle(e: Event) {
-    if (cropCount == null || cropDir == null) {
-        return;
-    }
-    xray.direction = cropDir.value;
-    let count = Number.parseInt(cropCount.value);
-    if (count < 0) {
-        count = 0;
-        cropCount.value = "0";
-    }
-    switch (xray.direction) {
-        case "up":
-        case "down":
-            if (count >= puzzleSize.y) {
-                count = puzzleSize.y - 1;
-                cropCount.value = count.toString();
-            }
-            break;
-        case "left":
-        case "right":
-            if (count >= puzzleSize.x) {
-                count = puzzleSize.x - 1;
-                cropCount.value = count.toString();
-            }
-            break;
-        case "front":
-        case "back":
-            if (count >= puzzleSize.z) {
-                count = puzzleSize.z - 1;
-                cropCount.value = count.toString();
-            }
-            break;
-    }
-    xray.count = count;
-
-    // Update visibility
-    updateVisibility();
-}
-
-function updateVisibility() {
+function updateVisibility(xray: XRay) {
     let x = 0;
     let y = 0;
     let z = 0;
     for (let cube of cubes) {
-        if (isVisible(xray, x, y, z, puzzleSize.x, puzzleSize.y, puzzleSize.z)) {
+        if (isVisible(xray, cube, puzzleSize.x, puzzleSize.y, puzzleSize.z)) {
             cube.visible = true;
             cube.layers.enable(0);
         } else {
@@ -281,41 +238,30 @@ function updateVisibility() {
     }
 }
 
-cropDir?.addEventListener("change", changeHandle);
-
-cropCount?.addEventListener("change", changeHandle);
-
 function animate() {
     requestAnimationFrame(animate);
 
+    const dragSpeed = 10;
+    const distance = dragSpeed * (pointer.x - startPosition.x);
     if (mouseOnHandle == "x") {
-        const minX = -puzzleSize.x / 2 - 1;
-        const maxX = minX + puzzleSize.x - 1;
-        const distance = 10 * (pointer.x - startPosition.x);
-        const newPosition = xOriginalPosition.x + distance - (distance % 1);
-        const clampedPosition = Math.max(minX, Math.min(newPosition, maxX));
-        
+        const newPosition = xOriginalPosition + Math.floor(distance);
+        const clampedPosition = clamp(newPosition, handleMinX, handleMaxX);
+
         if (clampedPosition != xHandleMesh.position.x) {
-            xray.direction = "left";
-            xray.count = clampedPosition - xOriginalPosition.x;
-            updateVisibility();
+            updateVisibility({ direction: "left", count: clampedPosition - xOriginalPosition });
         }
+
         xHandleMesh.position.setX(clampedPosition);
     } else if (mouseOnHandle == "z") {
-        const minZ = -puzzleSize.z / 2 - 1;
-        const maxZ = minZ + puzzleSize.z - 1;
-        const distance = 10 * (pointer.x - startPosition.x);
-        const newPosition = zOriginalPosition.z + distance - (distance % 1);
-        const clampedPosition = Math.max(minZ, Math.min(newPosition, maxZ));
-        
+        const newPosition = zOriginalPosition + Math.floor(distance);
+        const clampedPosition = clamp(newPosition, handleMinZ, handleMaxZ);
+
         if (clampedPosition != zHandleMesh.position.z) {
-            xray.direction = "back";
-            xray.count = clampedPosition - zOriginalPosition.z;
-            updateVisibility();
+            updateVisibility({ direction: "back", count: clampedPosition - zOriginalPosition });
         }
-        
+
         zHandleMesh.position.setZ(clampedPosition);
-    } else {
+    } else { 
         raycaster.setFromCamera(pointer, camera);
         raycaster.layers.set(0);
 
@@ -323,25 +269,23 @@ function animate() {
         if (intersects.length > 0) {
             if (intersects[0].object == xHandleMesh) {
                 xHandleMesh.material.opacity = 1;
+                zHandleMesh.material.opacity = 0.5;
             } else if (intersects[0].object == zHandleMesh) {
                 zHandleMesh.material.opacity = 1;
+                xHandleMesh.material.opacity = 0.5;
+            } else {
+                xHandleMesh.material.opacity = 0.5;
+                zHandleMesh.material.opacity = 0.5;
             }
             if (click) {
                 let object: CoolMesh = intersects[0].object as CoolMesh;
-                if (!flag && !remove && (object == xHandleMesh || object == zHandleMesh)) {
-                    mouseOnHandle = object == xHandleMesh ? "x" : "z";
-                    xOriginalPosition.set(xHandleMesh.position.x, xHandleMesh.position.y, xHandleMesh.position.z);
-                    zOriginalPosition.set(zHandleMesh.position.x, zHandleMesh.position.y, zHandleMesh.position.z);
-                    startPosition.set(pointer.x, pointer.y);
-                    controls.enableRotate = false;
-                }
                 let x: number = object.qX ?? 0;
                 let y: number = object.qY ?? 0;
                 let z: number = object.qZ ?? 0;
                 click = false;
                 if (flag) {
                     object.qFlag = !object.qFlag;
-                    updateMaterial(object);
+                    updateMaterial(object, loader, hints);
                 } else if (remove) {
                     if (!object?.qFlag) {
                         if (puzzle[x][y][z]) {
@@ -351,11 +295,17 @@ function animate() {
                                 counter.textContent = mistakeCount.toString();
                             }
                             object.qFlag = true;
-                            updateMaterial(object);
+                            updateMaterial(object, loader, hints);
                         } else {
                             scene.remove(object);
                         }
                     }
+                } else if (object == xHandleMesh || object == zHandleMesh) {
+                    mouseOnHandle = object == xHandleMesh ? "x" : "z";
+                    xOriginalPosition = xHandleMesh.position.x;
+                    zOriginalPosition = zHandleMesh.position.z;
+                    startPosition.set(pointer.x, pointer.y);
+                    controls.enableRotate = false;
                 }
             }
         } else {
