8b9f27733440eb55ccf5bdae67713036c28f95aa
jonot <48457507+jonot-cyber@users.noreply.github.com>
2023-09-19 15:04:05 +0000

    Antialiased graphics

--- main.ts
+++ main.ts
@@ -92,7 +92,7 @@ scene.background = new Color(240, 240, 240);
 const camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 
-const renderer = new WebGLRenderer();
+const renderer = new WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 const controls = new OrbitControls(camera, renderer.domElement);
