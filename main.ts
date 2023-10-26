480f909f920fd00b6b8bb8c2bfc31567c3633c24
jonot <48457507+jonot-cyber@users.noreply.github.com>
2023-10-26 17:27:12 +0000

    Re-anti-alias

--- main.ts
+++ main.ts
@@ -195,7 +195,7 @@ const scene = new Scene();
 scene.background = new Color(255, 255, 255);
 const camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-const renderer = new WebGLRenderer();
+const renderer = new WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
